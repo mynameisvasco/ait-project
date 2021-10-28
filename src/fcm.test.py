fcefcdd456349d4df5776a6b3fd6e163b1e4fd3c
Vasco Sousa <myemailvasco@gmail.com>
2021-10-28 16:19:35 +0100

    Refactored fcm.test.py

--- src/fcm.test.py
+++ src/fcm.test.py
@@ -1,14 +1,8 @@
 from fcm import Fcm
 
-fcm = Fcm("test", 0.01, 3)
-fcm.add_text("ababababa")
+fcm = Fcm("example.txt", 0.01, 3)
+fcm.add_file("example/example.txt")
 
 assert fcm.smoothing == 0.01
 assert fcm.context_size == 3
-assert fcm.get_all_contexts() == set((('a', 'b', 'b'), ('a', 'b', 'a'), ('b', 'b', 'b'), ('b', 'a', 'b'),
-                                      ('b', 'b', 'a'), ('b', 'a', 'a'), ('a', 'a', 'b'), ('a', 'a', 'a')))
-
-
-fcm = Fcm("example.txt", 0.01, 3)
-hash = fcm.add_file("example/example.txt")
 assert fcm.get_probability(" ", "the") == 0.5803758789221615
