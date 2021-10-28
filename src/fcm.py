cfe7c9bb4be3bab5f63c23e5c60717aeb9556ca6
Vasco Sousa <myemailvasco@gmail.com>
2021-10-28 14:24:10 +0100

    Added smoothing parameter

--- src/fcm.py
+++ src/fcm.py
@@ -4,7 +4,6 @@ from itertools import product
 from collections import defaultdict
 import pickle
 from hashlib import md5
-from math import log2
 
 
 class Fcm:
@@ -13,21 +12,19 @@ class Fcm:
     smoothing: float
     context_size: int
 
-    # Sigma = ["a", "b"]
-    # K = 2, contexto = ["aa", "ba", "ab", "bb"]
-
-    def __init__(self, symbols: Set[str], smoothing: float, context_size: int) -> None:
-        assert len(symbols) > 0
-        assert smoothing > 0
+    def __init__(self, smoothing: float, context_size: int) -> None:
+        assert smoothing > 0 and smoothing <= 1
         assert context_size > 0
 
         self.index = dict()
-        self.symbols = symbols
+        self.symbols = set()
         self.smoothing = smoothing
         self.context_size = context_size
 
-        for context in self.get_all_contexts():
-            self.index[''.join(context)] = defaultdict(int)
+    def hash_text(self, text: str):
+        hash = md5()
+        hash.update(text.encode("utf-8"))
+        return hash.hexdigest()
 
     def get_all_contexts(self) -> Set[Tuple[str]]:
         return set(product(self.symbols, repeat=self.context_size))
@@ -35,8 +32,8 @@ class Fcm:
     def get_probability(self, symbol: str, context: str):
         assert len(context) == self.context_size
 
-        res = self.index[context][symbol]
-        other = 0
+        res = self.index[context][symbol] + self.smoothing
+        other = self.smoothing * len(self.symbols)
 
         for key in self.index[context]:
             other += self.index[context][key]
@@ -49,26 +46,27 @@ class Fcm:
     def get_entropy(self, context: str):
         pass
 
-    def add_file(self, path: str):
-        assert exists(path)
+    def add_symbols_from_text(self, text: str):
+        self.symbols.update(sorted(set(text)))
 
+        for context in self.get_all_contexts():
+            self.index[''.join(context)] = defaultdict(int)
+
+    def add_file(self, path: str):
         file = open(path, "r")
         text = file.read()
-
-        hash = md5()
-        hash.update(text.encode("utf-8"))
-        hash = hash.hexdigest()
-
-        if self.load_cache(hash):
-            return
-
         self.add_text(text)
-        self.save_cache(hash)
 
     def add_text(self, text: str):
         assert len(text) > 0
 
         i = 0
+        hash = self.hash_text(text)
+        self.add_symbols_from_text(text)
+
+        if self.load_cache(hash):
+            return
+
         while i < len(text):
             if i + self.context_size >= len(text):
                 break
@@ -78,6 +76,8 @@ class Fcm:
             self.add_sequence(symbol, context)
             i += 1
 
+        self.save_cache(hash)
+
     def add_sequence(self, symbol: str, context: str):
         assert len(symbol) == 1
         assert len(context) == self.context_size
@@ -86,11 +86,15 @@ class Fcm:
         self.index[context][symbol] += 1
 
     def save_cache(self, hash: str):
+        assert len(hash) == 32
+
         path = f"cache/{hash}"
         file = open(path, "wb")
         pickle.dump(self.index, file)
 
     def load_cache(self, hash: str):
+        assert len(hash) == 32
+
         path = f"cache/{hash}"
 
         if not exists(path):
--- src/fcm.test.py
+++ src/fcm.test.py
@@ -1,7 +1,7 @@
-from string import printable
 from fcm import Fcm
 
-fcm = Fcm(["a", "b"], 0.01, 3)
+fcm = Fcm(0.01, 3)
+fcm.add_text("ababababa")
 
 assert fcm.smoothing == 0.01
 assert fcm.context_size == 3
@@ -9,6 +9,6 @@ assert fcm.get_all_contexts() == set((('a', 'b', 'b'), ('a', 'b', 'a'), ('b', 'b
                                       ('b', 'b', 'a'), ('b', 'a', 'a'), ('a', 'a', 'b'), ('a', 'a', 'a')))
 
 
-fcm = Fcm(printable, 0.01, 3)
+fcm = Fcm(0.01, 3)
 hash = fcm.add_file("example/example.txt")
-assert fcm.get_probability(" ", "the") == 0.5803788677312265
+assert fcm.get_probability(" ", "the") == 0.5803758789221615
